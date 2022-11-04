aaa7d8591e75a42675f9268985af81b2c77653ce
fahadhos <generalus159@gmail.com>
2022-11-04 20:27:10 +0600

    updated 4 nov 22

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$qq=10mj_vcdtn=9l&+67fdo4c-kwqiyyyfxaz%r)@3-xm*c1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://esyby.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','esyby.herokuapp.com']
 
 
 # Application definition
