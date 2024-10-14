2bb48916e54cad62ca74a71ab56fc1b30be72819
Fahad Hossain <59794435+fahadhos@users.noreply.github.com>
2024-10-14 23:03:47 +0600

    Update urls.py

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,7 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
+ 
+ 
+ 
 urlpatterns = [
+      path('', index),
     path('admin/', admin.site.urls),
     path('home/', include('easy_buy.urls')),
     # url(r'^media/(?P<path>.*)$', serve,
